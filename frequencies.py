176fe68ebe74b545e5e8a5b7905f2ad56f4b756e
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-09-29 20:12:19 +0100

    Add functionality

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if not isinstance(item, str):
+            item = str(item)
+        keys = frequencies.keys()
+
+        if item not in keys:
+            frequencies[item] = 1
+        else:
+            frequencies[item] = frequencies.get(item) + 1
     return frequencies
